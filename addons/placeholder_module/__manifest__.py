9a693f5c48cf88d42afab78d51dda4e72bc7bf08
Miguel Lozano <miggg89@gmail.com>
2025-11-29 18:28:18 +0100

    arreglada estructura de archivos

---
+++ addons/placeholder_module/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Placeholder Module',
+    'version': '18.0.1.0.0',
+    'summary': 'Placeholder to make addons directory valid',
+    'description': 'This module exists only to make the addons directory valid for Odoo',
+    'category': 'Hidden',
+    'depends': ['base'],
+    'installable': False,
+    'auto_install': False,
+}
